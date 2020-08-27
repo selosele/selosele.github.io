984373c074bd9f9c615c8624fa2c66397fde25f9
tidyred <youlb9@yandex.ru>
2020-08-27 18:41:33 +0900

    JS 컴파일 관련 수정

--- assets/js/_js.js
+++ assets/js/_js.js
@@ -1,6 +1,37 @@
-/* ==========================================================================
-   _function.js에서 작성한 함수 호출/재사용 불가능한 함수 모음
-   ========================================================================== */
+// anchor href와 현재 url 일치할경우 aria-current="page" 속성 추가
+var anchorSetAriaCurrent = function(anchorNode) {
+    if (!anchorNode) return;
+
+    for (var i = 0; i < anchorNode.length; i++) {
+        if (anchorNode[i].getAttribute("href") === location.href) {
+            anchorNode[i].setAttribute("aria-current", "page");
+        }
+    }
+};
+
+// 이미지 정렬
+var alignImg = function(elem) {
+    if (!elem) return;
+
+    for (var i = 0; i < elem.length; i++) {
+        if (elem[i].getBoundingClientRect().width > elem[i].getBoundingClientRect().height) {
+            elem[i].classList.add("image--horizontal");
+        } else {
+            elem[i].classList.add("image--vertical");
+        }
+    }
+};
+
+// inline 요소 여백 제거
+var removeWhiteSpace = function(parentElem) {
+    if (!parentElem) return;
+
+    for (var i = 0; i < parentElem.length; i++) {
+        Array.prototype.slice.call(parentElem[i].childNodes).forEach(function(childElem) {
+            if (childElem.nodeType === 3) parentElem[i].removeChild(childElem);
+        });
+    }
+};
 
 // IE 11 ~ 9 체크
 if (window.navigator.userAgent.toLowerCase().indexOf('trident') > -1) document.documentElement.classList.add("only-ie");

8377c773e7b1d2b5b1c20880e2883378e27a08f1
tidyred <youlb9@yandex.ru>
2020-09-19 22:10:31 +0900

    레이아웃 HTML/검색 플러그인 JS 수정

--- assets/js/lunr/lunr-en.js
+++ assets/js/lunr/lunr-en.js
@@ -45,22 +45,22 @@ $(document).ready(function() {
       if(store[ref].teaser){
         var searchitem =
           '<div class="list__item">'+
-            '<article class="archive__item" aria-labelledby="title-'+store[ref].aria+'" aria-describedby="desc-'+store[ref].aria+'" itemscope itemtype="https://schema.org/CreativeWork">'+
-              '<h2 class="archive__item-title" itemprop="headline">'+
-                '<a href="'+store[ref].url+'" id="title-'+store[ref].aria+'" rel="permalink">'+store[ref].title+'</a>'+
+            '<article aria-labelledby="title-'+store[ref].aria+'" aria-describedby="desc-'+store[ref].aria+'" itemscope itemtype="https://schema.org/CreativeWork" class="archive__item">'+
+              '<h2 class="archive__item-title">'+
+                '<a href="'+store[ref].url+'" rel="permalink" id="title-'+store[ref].aria+'">'+store[ref].title+'</a>'+
               '</h2>'+
-              '<p class="archive__item-summary" id="desc-'+store[ref].aria+'" itemprop="description">'+store[ref].summary.split(" ").splice(0,20).join(" ")+'...</p>'+
+              '<p itemprop="description" id="desc-'+store[ref].aria+'" class="archive__item-summary">'+store[ref].summary.split(" ").splice(0,20).join(" ")+'...</p>'+
             '</article>'+
           '</div>';
       }
       else{
     	  var searchitem =
           '<div class="list__item">'+
-            '<article class="archive__item" aria-labelledby="title-'+store[ref].aria+'" aria-describedby="desc-'+store[ref].aria+'" itemscope itemtype="https://schema.org/CreativeWork">'+
-              '<h2 class="archive__item-title" itemprop="headline">'+
-                '<a href="'+store[ref].url+'" id="title-'+store[ref].aria+'" rel="permalink">'+store[ref].title+'</a>'+
+            '<article aria-labelledby="title-'+store[ref].aria+'" aria-describedby="desc-'+store[ref].aria+'" itemscope itemtype="https://schema.org/CreativeWork" class="archive__item">'+
+              '<h2 class="archive__item-title">'+
+                '<a href="'+store[ref].url+'" rel="permalink" id="title-'+store[ref].aria+'">'+store[ref].title+'</a>'+
               '</h2>'+
-              '<p class="archive__item-summary" id="desc-'+store[ref].aria+'" itemprop="description">'+store[ref].summary.split(" ").splice(0,20).join(" ")+'...</p>'+
+              '<p itemprop="description" id="desc-'+store[ref].aria+'" class="archive__item-summary">'+store[ref].summary.split(" ").splice(0,20).join(" ")+'...</p>'+
             '</article>'+
           '</div>';
       }
